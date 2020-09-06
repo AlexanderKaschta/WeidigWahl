e82c72c631a854b190a54943a0018469e1d3a58d
AlexanderKaschta <alexander.kaschta@t-online.de>
2020-09-06 22:25:22 +0200

    - Überarbeitung des Footers

--- core/include/footer.php
+++ core/include/footer.php
@@ -1,13 +1,25 @@
 <?php
-    require_once("core/config.php");
-    $creation_year = RELEASE_YEAR;
-    $current_year = date("Y");
+require_once("core/config.php");
+$creation_year = RELEASE_YEAR;
+$current_year = date("Y");
 ?>
 
 <footer class="text-muted">
     <div class="container">
-        <p>WeidigWahl | Ein Wahlsystem für die Weidigschule</p>
+
+        <p><b>WeidigWahl | Ein Wahlsystem für die Weidigschule</b></p>
+        <p>&copy; <?php echo $creation_year . (($creation_year != $current_year) ? '-' . $current_year : '') . " " . HOSTER; ?></p>
         <hr class="col-xs-12">
-        &copy; <?php echo $creation_year . (($creation_year != $current_year) ? '-' . $current_year : '')." ".HOSTER; ?>
+        <div class="row">
+            <div class="col-md-4 ">
+                <a href="#">GitHub</a>
+            </div>
+            <div class="col-md-4">
+                <a href="#" class="text-muted">Impressum & Datenschutz</a>
+            </div>
+            <div class="col-md-4">
+                <a href="#">Schulwebseite</a>
+            </div>
+        </div>
     </div>
 </footer>
